302b8b21a74633ca34d45a6edc481d7f3a762a68
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-02-22 14:41:56 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,34 +17,55 @@ window.onload = function () {
 	var success = false;
 	var href = window.location.href;
 	if (href.includes('?') && href.includes('=')) {
+		var ch = null;
+		var mode = null;
 		var params = processParams(href);
 		for (var i = 0; i < params.length; i++) {
-			if (params[i].name == 'ch') {
-				var ch = params[i].value;
-				if (twitchIDRegex.test(ch)) {
-					// Create a Twitch.Embed object that will render within the "twitch-embed" element
-					new Twitch.Player("twitch-embed", {
-						width: "100%",
-						height: "100%",
-						channel: ch,
-						// layout: "video-with-chat",
-						// Only needed if this page is going to be embedded on other websites
-						// parent: ["embed.example.com", "othersite.example.com"],
-						autoplay: true,
-						muted: false
-					});
-					
-					success = true;
-				}
+			if (params[i].name == 'ch' && twitchIDRegex.test(params[i].value)) ch = params[i].value;
+			if (params[i].name == 'mode') mode = params[i].value;
+		}
+		
+		if (ch != null && mode != null) {
+			// Create embed object that will render within the "twitch" element
+			if (mode == 'player') {
+				new Twitch.Player("twitch", {
+					width: "100%",
+					height: "100%",
+					channel: ch,
+					// layout: "video-with-chat",
+					// Only needed if this page is going to be embedded on other websites
+					// parent: ["embed.example.com", "othersite.example.com"],
+					autoplay: true,
+					muted: false
+				});
+				
+				success = true;
+			}
+			else if (mode == 'embed') {
+				new Twitch.Embed("twitch", {
+					width: "100%",
+					height: "100%",
+					channel: ch,
+					// layout: "video-with-chat",
+					// Only needed if this page is going to be embedded on other websites
+					// parent: ["embed.example.com", "othersite.example.com"],
+					autoplay: true,
+					muted: false
+				});
 			}
 		}
 	}
 	
 	// Failed to embed a channel
 	if (!success) {
-		document.getElementById('twitch-embed').innerHTML = '<form class="myform" method="get">' +
+		document.getElementById('twitch').innerHTML = '<form class="myform" method="get">' +
 				'<div class="myclass">' +
-					'Twitch Channel ID: <input type="text" id="ch" name="ch"> <input type="submit" value="Open">' +
+					'Twitch Channel ID: <input type="text" id="ch" name="ch"> ' +
+					'<select id="mode" name="mode">' +
+						'<option value="player">Player Only</option>' +
+						'<option value="embed">Full Embed</option>' +
+					'</select> ' +
+					'<input type="submit" value="Open">' +
 				'</div>' +
 			'</form>';
 		document.getElementById('ch').focus();
